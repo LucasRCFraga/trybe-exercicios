c6ea38e3049eb9c4aac09670a8962e8550c546b4
lucasrcfraga <lucasrcfraga@gmail.com>
2023-04-01 20:54:02 -0300

    Pontos 1 e 2 finalizados e comeco do ponto 3

---
+++ fundamentos/2-introducao-javaScript/3.1-primeiros-passos/script.js
@@ -0,0 +1,36 @@
+let a = null;
+let b = null;
+
+console.log();
+
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// Ponto 1 finalizado;
+
+
+
+if(a > b){
+    console.log('a é maior do que b');
+}
+else {
+    console.log('b é maior do que a');
+}
+
+// Ponto 2 finalizado;
+
+let c = null;
+
+if(){
+
+}
+if else{
+
+} else{
+
+}
+
+// Ponto 3 finalizado
