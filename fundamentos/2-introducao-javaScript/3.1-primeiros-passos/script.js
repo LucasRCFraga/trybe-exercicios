17f74017ed54960c0087a684138394514e57e57f
lucasrcfraga <lucasrcfraga@gmail.com>
2023-04-01 21:23:12 -0300

    Finalizado pontos 3/4/5, checagem de logica no gabarito do ponto 5

--- fundamentos/2-introducao-javaScript/3.1-primeiros-passos/script.js
+++ fundamentos/2-introducao-javaScript/3.1-primeiros-passos/script.js
@@ -24,13 +24,60 @@ else {
 
 let c = null;
 
-if(){
-
-}
-if else{
-
+if(a > b && a > c){
+    console.log('O maior número é: '+ a);
+} else if(b > a && b > c){
+    console.log('O maior número é: ' + b);
 } else{
-
+    console.log('O maior número é: ' + c);
 }
 
-// Ponto 3 finalizado
+// Ponto 3 finalizado;
+
+let d;
+
+if(d > 0){
+    console.log('Positive');
+} else if(d < 0){
+    console.log('Negative');
+}else{
+    console.log('Zero');
+}
+
+// Ponto 4 finalizado;
+
+let q = 80;
+let w = 77;
+let e = 92;
+let somaTri = q + w + e;
+let checkTri = q > 0 && w > 0 && e > 0;
+// let triangulo = null;
+
+if(checkTri){
+    if (somaTri === 180){
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
+} else{
+    console.log('Triangulo invalido');
+}
+
+// VVVVVVVVVVVVVVV Esse foi o que eu fiz, estava correto mas
+// precisava de um if dentro de outro if, pois nao é possivel ter
+// um triangulo com angulos negativos, entao precisa checar isso primeiro
+//
+//
+// if(somaTri <= 180){
+//     triangulo = true;
+//     console.log(triangulo);
+// } else if(somaTri > 180){
+//     triangulo = false;
+//     console.log();
+// } else{
+//     console.log();
+// }
+
+// Ponto 5 finalizado;
+
