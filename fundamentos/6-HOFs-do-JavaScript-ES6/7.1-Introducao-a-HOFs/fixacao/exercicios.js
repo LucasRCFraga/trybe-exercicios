00446790d0b65d0beeedbef58348c3c0e46e74dc
lucasrcfraga <lucasrcfraga@gmail.com>
2023-05-05 16:18:45 -0300

    Fixacao 1 finalizado

--- fundamentos/6-HOFs-do-JavaScript-ES6/7.1-Introducao-a-HOFs/fixacao/exercicios.js
+++ fundamentos/6-HOFs-do-JavaScript-ES6/7.1-Introducao-a-HOFs/fixacao/exercicios.js
@@ -1,7 +1,7 @@
 /*
  * PARA FIXAR *
+*/
 
- Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
- */
+// Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
 const numbers = [19, 21, 30, 3, 45, 22, 15];
-
+const verify = numbers.find((N) => N % 3 === 0 && N % 5 === 0)
