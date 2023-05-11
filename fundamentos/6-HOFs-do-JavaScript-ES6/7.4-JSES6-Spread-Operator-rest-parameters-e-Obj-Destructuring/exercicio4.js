7d98b13cbbaf2e97f9ff41272cc17941d8a400fe
lucasrcfraga <lucasrcfraga@gmail.com>
2023-05-11 12:51:11 -0300

    Exercicio 4 completo

--- fundamentos/6-HOFs-do-JavaScript-ES6/7.4-JSES6-Spread-Operator-rest-parameters-e-Obj-Destructuring/exercicio4.js
+++ fundamentos/6-HOFs-do-JavaScript-ES6/7.4-JSES6-Spread-Operator-rest-parameters-e-Obj-Destructuring/exercicio4.js
@@ -34,3 +34,18 @@ const people = [
 ];
 
 // escreva filterPeople abaixo
+const filter = (people) => {
+   return people.filter((nat) => nat.nationality === 'Australian' && nat.bornIn > 1900 & nat.bornIn <= 2000);
+}
+
+console.log(filter(people));
+
+// * Resposta abaixo utiliza o Obj destructuring dentro do parametro do .filter()
+
+const filterPeople = (arr) => {
+    return people.filter(({ nationality, bornIn }) => {
+      return nationality === 'Australian' && bornIn > 1900 & bornIn <= 2000;
+    });
+}
+
+console.log(filterPeople(people));
