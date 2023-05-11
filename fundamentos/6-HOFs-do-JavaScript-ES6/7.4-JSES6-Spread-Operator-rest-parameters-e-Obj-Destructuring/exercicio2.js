b020610758a3845338af79c4b25dd0bf36b947fc
lucasrcfraga <lucasrcfraga@gmail.com>
2023-05-11 12:06:50 -0300

    Exercicio 2 completo

---
+++ fundamentos/6-HOFs-do-JavaScript-ES6/7.4-JSES6-Spread-Operator-rest-parameters-e-Obj-Destructuring/exercicio2.js
@@ -0,0 +1,12 @@
+/**
+ * Crie uma função sum que, dado um número ilimitado de 
+ * parâmetros, retorna a soma desses parâmetros. Ao chamar 
+ * a função dessa forma : sum(4,5,6), o número 15 deve ser retornado.
+ */
+
+
+
+// escreva sum abaixo
+const sum  = (...item) => item.reduce((acc, curr) => acc + curr);
+
+console.log(sum(4,5,6));
