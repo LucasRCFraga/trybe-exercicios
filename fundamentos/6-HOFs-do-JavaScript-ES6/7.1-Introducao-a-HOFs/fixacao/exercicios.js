ce5abf57d97b816223c0cb64815ac5303703149e
lucasrcfraga <lucasrcfraga@gmail.com>
2023-05-11 11:39:57 -0300

    Finalizado dia 7.1

--- fundamentos/6-HOFs-do-JavaScript-ES6/7.1-Introducao-a-HOFs/fixacao/exercicios.js
+++ fundamentos/6-HOFs-do-JavaScript-ES6/7.1-Introducao-a-HOFs/fixacao/exercicios.js
@@ -1,6 +1,5 @@
 /*
  * PARA FIXAR *
-*/
 
 // Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
 const numbers = [19, 21, 30, 3, 45, 22, 15];
@@ -21,4 +20,100 @@ const musicas = [
 ];
   
 const musicasF = musicas.find((find) => find.id === '31031685');
+*/
 
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65},
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 }, 
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const resultadoGeral = estudantes.map((element) => {
+  // const notaGeral = element.materias;
+  const notaAluno = element.materias.reduce((acc, notaDoAluno) => acc.nota > notaDoAluno.nota ? acc : notaDoAluno);
+
+  return {
+      name: element.nome,
+      materia: notaAluno.name,
+  };
+});
+
+console.log(resultadoGeral);
+
+const getBestClass = (acc, materia) => (acc.nota > materia.nota) ? acc : materia;
+
+const reportBest = (students) => students.map((student) => ({
+  name: student.nome,
+  materia: student.materias.reduce(getBestClass).name,
+}));
+
+console.log(reportBest(estudantes));
