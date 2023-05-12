f5571ba05ee60bb2d357822ef7ec1e79d2ee6260
lucasrcfraga <lucasrcfraga@gmail.com>
2023-05-12 08:03:54 -0300

    Exercise 4 completed

--- fundamentos/6-HOFs-do-JavaScript-ES6/7.5-JSES6-Array,Default-Destructuring-Obj-property-shorhand-e-Default-parameters/exercicio4.js
+++ fundamentos/6-HOFs-do-JavaScript-ES6/7.5-JSES6-Array,Default-Destructuring-Obj-property-shorhand-e-Default-parameters/exercicio4.js
@@ -16,6 +16,7 @@ const student2 = {
 }
   
 // escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
+const getLastName = ({ lastName = 'lastName não preenchido'}) =>  lastName;
   
 console.log(getLastName(student1));
 console.log(getLastName(student2));
